530785ddb552f2d64142c262ecc5f4e488a07e44
Sam Privett <privetsl@mail.uc.edu>
2015-12-16 20:21:34 -0500

    Changed output to server to send bytes instead of strings.

--- src/client/VendingMachine.java
+++ src/client/VendingMachine.java
@@ -9,11 +9,11 @@ import util.Util;
 
 public class VendingMachine{
 
-	DataOutputStream toServer;
+	OutputStream toServer;
 	//ObjectInputStream isFromServer;
 
 	String chat;
-	static String ip = "localhost";
+	static String ip = "192.168.1.125";
 	
 	//Current format is P:MM-MM-MM-MM-MM-MM:XX 
 	//P = Protocol, X = ItemSlot(A1, A2...), and M = MacAddress
@@ -39,7 +39,7 @@ public class VendingMachine{
 			@SuppressWarnings("resource")
 			Socket connectToServer = new Socket(ip, 8000);
 
-			toServer = new DataOutputStream(connectToServer.getOutputStream());
+			toServer = connectToServer.getOutputStream();
 			toServer.flush();
 			//isFromServer = new ObjectInputStream(connectToServer.getInputStream());
 			System.out.println("Connected!");
@@ -77,7 +77,7 @@ public class VendingMachine{
 								//grab message to send and physical address of the client
 								String message =(Protocol + ":" + MacAddress + ":" + ItemSold);
 
-								toServer.writeUTF(message);
+								writeToServer(message, toServer);
 								toServer.flush(); // clear path
 								System.exit(0); //close program if user typed bye
 							}
@@ -85,7 +85,7 @@ public class VendingMachine{
 							//grab message to send and physical address of the client
 							String message =(Protocol + ":" + MacAddress + ":" + ItemSold);
 
-							toServer.writeUTF(message);
+							writeToServer(message, toServer);
 							toServer.flush(); // clear path
 						}else{
 							//TO-DO throw an error to server saying the entered ItemSold was null and it cannot be null
@@ -123,10 +123,27 @@ public class VendingMachine{
 	
 	private void startUpMessage(){
 		try {
-			toServer.writeUTF(Protocol + ":" + MacAddress + ":" + ItemSold);
+			writeToServer(Protocol + ":" + MacAddress + ":" + ItemSold, toServer);
 			toServer.flush();
 			Protocol = "1";
 		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeToServer(String msg, OutputStream toServer) throws IOException {
+		try {
+			byte[] toSendBytes = msg.getBytes();
+			int toSendLen = toSendBytes.length;
+			byte[] toSendLenBytes = new byte[4];
+			toSendLenBytes[0] = (byte)(toSendLen & 0xff);
+			toSendLenBytes[1] = (byte)((toSendLen >> 8) & 0xff);
+			toSendLenBytes[2] = (byte)((toSendLen >> 16) & 0xff);
+			toSendLenBytes[3] = (byte)((toSendLen >> 24) & 0xff);
+			toServer.write(toSendLenBytes);
+			toServer.write(toSendBytes);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
